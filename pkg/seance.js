5b1fca4252ff764f8913f82ab9fadb84758dac0f
UberMetroid <UberMetroid@users.noreply.github.com>
2026-09-16 00:35:41 +1000

    wasm: gauge sits on the top semicircle

--- pkg/seance.js
+++ pkg/seance.js
@@ -1,5 +1,5 @@
 /* @ts-self-types="./seance.d.ts" */
-import * as import1 from "./snippets/seance-bb923ec93ca7df41/inline0.js"
+import * as import1 from "./snippets/seance-cde660e739ea3338/inline0.js"
 
 
 /**
@@ -64,7 +64,7 @@ function __wbg_get_imports() {
     return {
         __proto__: null,
         "./seance_bg.js": import0,
-        "./snippets/seance-bb923ec93ca7df41/inline0.js": import1,
+        "./snippets/seance-cde660e739ea3338/inline0.js": import1,
     };
 }
 
